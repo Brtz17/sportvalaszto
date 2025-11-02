16863736f2932f00e15d5065adbdc6d2eb2008b3
Beni <beni@pop-os.lan>
2025-11-02 14:21:04 +0100

    kerlekkk

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        signup: resolve(__dirname, 'signup.html')
+      }
+    }
   },
   publicDir: 'public',
   server: {
     port: 3000
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'js/**/*',
+          dest: 'js'
+        },
+        {
+          src: 'style/**/*',
+          dest: 'style'
+        }
+        // A hatter.png-t hagyd ki innen, mert már a public mappában van
+      ]
+    })
+  ]
 });
